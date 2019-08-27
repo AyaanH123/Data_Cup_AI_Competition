701cf28db11aac15fd0936f2fdf4645a44e1c866
AyaanH123 <46852595+AyaanH123@users.noreply.github.com>
2019-08-27 14:49:29 -0400

    Add files via upload
    Check

---
+++ File_Handler.py
@@ -0,0 +1,4 @@
+import Json_Read.py as jr
+
+jr.create_dataframes('True North AI')
+jr.create_dataframes2('True North AI')
--- Json_Read.py
+++ Json_Read.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+
 def create_dataframes(path_to_TrueNorthAI):
     path = path_to_TrueNorthAI
     df1 = pd.read_json(path+"/train.json")
@@ -8,6 +9,7 @@ def create_dataframes(path_to_TrueNorthAI):
     cols = ['claim', 'claimant', 'date', 'related_articles', 'label']
     df1 = df1[cols]
     print(df1)
+    
 def create_dataframes2(path_to_TrueNorthAI):
     path = path_to_TrueNorthAI
     article_id = []
@@ -16,13 +18,13 @@ def create_dataframes2(path_to_TrueNorthAI):
         article_id.append(filename.split('.')[0])
         with open(path+"/train_articles/"+filename, encoding="utf8") as f_input:
             article_content.append(f_input.read())
+            print("article complete")
+    
     data = {'ID': article_id, 'Content': article_content}
     df2 = pd.DataFrame(data)
     df2.set_index('ID', inplace=True)
-    print(df2)
-    
-if __name__ == "__main__":
-    create_dataframes('C:/Users/dhruve/Desktop/TrueNorthAI')
-    create_dataframes2('C:/Users/dhruve/Desktop/TrueNorthAI')
-        
+    print(df2)   
 
+if __name__ == "__main__":
+    create_dataframes('True North AI')
+    create_dataframes2('True North AI')
