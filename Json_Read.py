c0066a49eb79259e5d5d96fc9d3e47b2a3b8ef48
dhruvpatel19 <53876897+dhruvpatel19@users.noreply.github.com>
2019-08-27 15:30:00 -0400

    Add files via upload

---
+++ Json_Read.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import os 
+
+def create_dataframes(path_to_TrueNorthAI):
+    path = path_to_TrueNorthAI
+    df1 = pd.read_json(path+"/train.json")
+    df1.set_index('id', inplace=True)
+    cols = df1.columns.tolist()
+    cols = ['claim', 'claimant', 'date', 'related_articles', 'label']
+    df1 = df1[cols]
+    return df1
+def create_dataframes2(path_to_TrueNorthAI):
+    path = path_to_TrueNorthAI
+    article_id = []
+    article_content = []
+    for filename in os.listdir(path+"/train_articles/"):
+        article_id.append(filename.split('.')[0])
+        with open(path+"/train_articles/"+filename, encoding="utf8") as f_input:
+            article_content.append(f_input.read())
+    data = {'ID': article_id, 'Content': article_content}
+    df2 = pd.DataFrame(data)
+    df2.set_index('ID', inplace=True)
+    return df2
+    
+if __name__ == "__main__":
+    create_dataframes('../TrueNorthAI')
+    create_dataframes2('../TrueNorthAI')
+        
+
