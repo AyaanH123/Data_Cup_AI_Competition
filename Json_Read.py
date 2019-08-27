b8a55e18b00c7d95179685332509dd30de1efdfe
AyaanH123 <46852595+AyaanH123@users.noreply.github.com>
2019-08-27 15:07:37 -0400

    Add files via upload
    Anti_spam

--- Json_Read.py
+++ Json_Read.py
@@ -8,7 +8,6 @@ def create_dataframes(path_to_TrueNorthAI):
     cols = df1.columns.tolist()
     cols = ['claim', 'claimant', 'date', 'related_articles', 'label']
     df1 = df1[cols]
-    print(df1)
     
 def create_dataframes2(path_to_TrueNorthAI):
     path = path_to_TrueNorthAI
@@ -18,13 +17,11 @@ def create_dataframes2(path_to_TrueNorthAI):
         article_id.append(filename.split('.')[0])
         with open(path+"/train_articles/"+filename, encoding="utf8") as f_input:
             article_content.append(f_input.read())
-            print("article complete")
     
     data = {'ID': article_id, 'Content': article_content}
     df2 = pd.DataFrame(data)
-    df2.set_index('ID', inplace=True)
-    print(df2)   
+    df2.set_index('ID', inplace=True) 
 
 if __name__ == "__main__":
-    create_dataframes('True North AI')
-    create_dataframes2('True North AI')
+    create_dataframes('../True North AI')
+    create_dataframes2('../True North AI')
