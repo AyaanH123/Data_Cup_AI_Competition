a161090f0c8f55cbe3cd8844135573889c9adacd
AyaanH123 <46852595+AyaanH123@users.noreply.github.com>
2019-08-28 10:46:22 -0400

    Add files via upload
    Added ML

--- Data_Clean.py
+++ Data_Clean.py
@@ -1,9 +1,95 @@
 import Json_Read as jr
+import pandas as pd
+import numpy as np
+from sklearn import model_selection, preprocessing, linear_model, naive_bayes, metrics, svm
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn import decomposition, ensemble
 
-df1 = jr.create_dataframes('../True North AI')
-df2 = jr.create_dataframes2('../True North AI')
-df3 = jr.create_dataframes_claim_label('../True North AI')
+import string
+from keras.preprocessing import text, sequence
+from keras import layers, models, optimizers
 
-print(df1)
-print(df2)
-print(df3)
+df_two_fields = jr.create_dataframes_claim_label('../True North AI')
+# split the dataset into training and validation datasets 
+train_x, valid_x, train_y, valid_y = model_selection.train_test_split(df_two_fields['claim'], df_two_fields['label'])
+
+# label encode the target variable 
+encoder = preprocessing.LabelEncoder()
+train_y = encoder.fit_transform(train_y)
+valid_y = encoder.fit_transform(valid_y)
+
+# create a count vectorizer object 
+count_vect = CountVectorizer(analyzer='word', token_pattern=r'\w{1,}')
+count_vect.fit(df_two_fields['claim'])
+
+# transform the training and validation data using count vectorizer object
+xtrain_count =  count_vect.transform(train_x)
+xvalid_count =  count_vect.transform(valid_x)
+
+# word level tf-idf
+tfidf_vect = TfidfVectorizer(analyzer='word', token_pattern=r'\w{1,}', max_features=5000)
+tfidf_vect.fit(df_two_fields['claim'])
+xtrain_tfidf =  tfidf_vect.transform(train_x)
+xvalid_tfidf =  tfidf_vect.transform(valid_x)
+
+# ngram level tf-idf 
+tfidf_vect_ngram = TfidfVectorizer(analyzer='word', token_pattern=r'\w{1,}', ngram_range=(2,3), max_features=5000)
+tfidf_vect_ngram.fit(df_two_fields['claim'])
+xtrain_tfidf_ngram =  tfidf_vect_ngram.transform(train_x)
+xvalid_tfidf_ngram =  tfidf_vect_ngram.transform(valid_x)
+
+# characters level tf-idf
+tfidf_vect_ngram_chars = TfidfVectorizer(analyzer='char', token_pattern=r'\w{1,}', ngram_range=(2,3), max_features=5000)
+tfidf_vect_ngram_chars.fit(df_two_fields['claim'])
+xtrain_tfidf_ngram_chars =  tfidf_vect_ngram_chars.transform(train_x) 
+xvalid_tfidf_ngram_chars =  tfidf_vect_ngram_chars.transform(valid_x) 
+
+# load the pre-trained word-embedding vectors 
+embeddings_index = {}
+for i, line in enumerate(open('data/wiki-news-300d-1M.vec')):
+    values = line.split()
+    embeddings_index[values[0]] = np.asarray(values[1:], dtype='float32')
+
+token = text.Tokenizer()
+token.fit_on_texts(df_two_fields['claim'])
+word_index = token.word_index
+
+# convert text to sequence of tokens and pad them to ensure equal length vectors 
+train_seq_x = sequence.pad_sequences(token.texts_to_sequences(train_x), maxlen=70)
+valid_seq_x = sequence.pad_sequences(token.texts_to_sequences(valid_x), maxlen=70)
+
+# create token-embedding mapping
+embedding_matrix = np.zeros((len(word_index) + 1, 300))
+for word, i in word_index.items():
+    embedding_vector = embeddings_index.get(word)
+    if embedding_vector is not None:
+        embedding_matrix[i] = embedding_vector
+        
+def train_model(classifier, feature_vector_train, label, feature_vector_valid, is_neural_net=False):
+    classifier.fit(feature_vector_train, label)
+    predictions = classifier.predict(feature_vector_valid)
+    if is_neural_net:
+        predictions = predictions.argmax(axis=-1)
+    
+    return metrics.accuracy_score(predictions, valid_y)
+
+#Naive Bayes
+accuracy = train_model(naive_bayes.MultinomialNB(), xtrain_count, train_y, xvalid_count)
+print("NB, Count Vectors: ", accuracy)
+
+accuracy = train_model(naive_bayes.MultinomialNB(), xtrain_tfidf, train_y, xvalid_tfidf)
+print("NB, WordLevel TF-IDF: ", accuracy)
+
+accuracy = train_model(naive_bayes.MultinomialNB(), xtrain_tfidf_ngram, train_y, xvalid_tfidf_ngram)
+print("NB, N-Gram Vectors: ", accuracy)
+
+accuracy = train_model(naive_bayes.MultinomialNB(), xtrain_tfidf_ngram_chars, train_y, xvalid_tfidf_ngram_chars)
+print("NB, CharLevel Vectors: ", accuracy)
+
+#df1 = jr.create_dataframes('../True North AI')
+#df2 = jr.create_dataframes2('../True North AI')
+
+
+#print(df1)
+#print(df2)
+#print(df_two_fields)
