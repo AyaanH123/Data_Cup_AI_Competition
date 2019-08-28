2526fe2813c52f60b418e755223a648cfed5be22
AyaanH123 <46852595+AyaanH123@users.noreply.github.com>
2019-08-28 10:22:43 -0400

    Add files via upload

--- Json_Read.py
+++ Json_Read.py
@@ -1,14 +1,24 @@
 import pandas as pd
-import os 
+import os
 
 def create_dataframes(path_to_TrueNorthAI):
     path = path_to_TrueNorthAI
-    df1 = pd.read_json(path+"/train.json")
-    df1.set_index('id', inplace=True)
-    cols = df1.columns.tolist()
+    df = pd.read_json(path+"/train.json")
+    df.set_index('id', inplace=True)
+    cols = df.columns.tolist()
+    cols = ['claim', 'claimant', 'date', 'related_articles', 'label']
+    df = df[cols]
+    return df
+
+def create_dataframes_claim_label(path_to_TrueNorthAI):
+    path = path_to_TrueNorthAI
+    df = pd.read_json(path+"/train.json")
+    df.set_index('id', inplace=True)
+    cols = df.columns.tolist()
     cols = ['claim', 'label']
-    df1 = df1[cols]
-    return df1
+    df = df[cols]
+    return df
+    
 def create_dataframes2(path_to_TrueNorthAI):
     path = path_to_TrueNorthAI
     article_id = []
@@ -17,13 +27,13 @@ def create_dataframes2(path_to_TrueNorthAI):
         article_id.append(filename.split('.')[0])
         with open(path+"/train_articles/"+filename, encoding="utf8") as f_input:
             article_content.append(f_input.read())
-    data = {'ID': article_id, 'Content': article_content}
-    df2 = pd.DataFrame(data)
-    df2.set_index('ID', inplace=True)
-    return df2
     
-if __name__ == "__main__":
-    create_dataframes('../TrueNorthAI')
-    create_dataframes2('../TrueNorthAI')
-        
+    data = {'ID': article_id, 'Content': article_content}
+    df = pd.DataFrame(data)
+    df.set_index('ID', inplace=True) 
+    return df
 
+if __name__ == "__main__":
+    create_dataframes('../True North AI')
+    create_dataframes2('../True North AI')
+    create_dataframes_claim_label('../True North AI')
