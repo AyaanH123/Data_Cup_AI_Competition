3ff2e800c7680dc4adc276aa7231c52e53d72276
AyaanH123 <46852595+AyaanH123@users.noreply.github.com>
2019-08-28 09:25:13 -0400

    Add files via upload

--- Data_Clean.py
+++ Data_Clean.py
@@ -1,35 +1,36 @@
 import Json_Read as jr
 import pandas as pd
+import numpy as np
+import nltk
+from nltk.stem import SnowballStemmer
+from nltk.stem.porter import PorterStemmer
+from nltk.tokenize import word_tokenize
+import seaborn as sb
 
 df1 = jr.create_dataframes('../True North AI')
 df2 = jr.create_dataframes2('../True North AI')
 
 
+
+
+
+eng_stemmer = SnowballStemmer('english')
+stopwords = set(nltk.corpus.stopwords.words('english'))
+
 #Stemming
-
 def stem_tokens(tokens, stemmer):
-
     stemmed = []
-
     for token in tokens:
-
         stemmed.append(stemmer.stem(token))
-
     return stemmed
 
 
 #process the data
-
 def process_data(data,exclude_stopword=True,stem=True):
-
     tokens = [w.lower() for w in data]
-
     tokens_stemmed = tokens
-
     tokens_stemmed = stem_tokens(tokens, eng_stemmer)
-
     tokens_stemmed = [w for w in tokens_stemmed if w not in stopwords ]
-
     return tokens_stemmed
 
 
@@ -38,55 +39,32 @@ def process_data(data,exclude_stopword=True,stem=True):
 #unigram 
 
 def create_unigram(words):
-
     assert type(words) == list
-
     return words
 
-
-
 #bigram
-
 def create_bigrams(words):
-
     assert type(words) == list
-
     skip = 0
-
     join_str = " "
-
     Len = len(words)
-
     if Len > 1:
-
         lst = []
-
         for i in range(Len-1):
-
             for k in range(1,skip+2):
-
                 if i+k < Len:
-
                     lst.append(join_str.join([words[i],words[i+k]]))
-
     else:
-
         #set it as unigram
-
         lst = create_unigram(words)
-
     return lst
 
 def tokenizer(text):
-
     return text.split()
 
-
-
-
+porter = PorterStemmer()
 
 def tokenizer_porter(text):
-
     return [porter.stem(word) for word in text.split()]
 
 print(df1)
