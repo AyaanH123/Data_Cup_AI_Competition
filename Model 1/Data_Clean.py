7fb0b0ddddbc5040a0cfb07d3491cbd932337b31
AyaanH123 <46852595+AyaanH123@users.noreply.github.com>
2019-08-30 14:58:42 -0400

    Add files via upload

---
+++ Model 1/Data_Clean.py
@@ -0,0 +1,75 @@
+import Json_Read as jr
+import pandas as pd
+import numpy as np
+from sklearn import model_selection, preprocessing, linear_model, naive_bayes, metrics, svm
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn import decomposition, ensemble
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+
+#training data frame
+df_two_fields = jr.create_dataframes_claim_label('/usr/src')
+df_input = jr.create_dataframes_input('/usr/local/dataset​')
+
+# split the dataset into training and validation datasets 
+#train_x, valid_x, train_y, valid_y = model_selection.train_test_split(df_two_fields['claim'], df_two_fields['label'])
+
+train_x = df_two_fields['claim']
+train_y = df_two_fields['label']
+valid_x = df_input['claim']
+#valid_y = df_input['label']
+
+# label encode the target variable 
+encoder = preprocessing.LabelEncoder()
+train_y = encoder.fit_transform(train_y)
+#valid_y = encoder.fit_transform(valid_y)
+
+# create a count vectorizer object 
+count_vect = CountVectorizer(analyzer='word', token_pattern=r'\w{1,}')
+count_vect.fit(df_two_fields['claim'])
+
+# transform the training and validation data using count vectorizer object
+xtrain_count =  count_vect.transform(train_x)
+xvalid_count =  count_vect.transform(valid_x)
+
+# word level tf-idf
+tfidf_vect = TfidfVectorizer(analyzer='word', token_pattern=r'\w{1,}', max_features=5000)
+tfidf_vect.fit(df_two_fields['claim'])
+xtrain_tfidf =  tfidf_vect.transform(train_x)
+xvalid_tfidf =  tfidf_vect.transform(valid_x)
+
+# ngram level tf-idf 
+tfidf_vect_ngram = TfidfVectorizer(analyzer='word', token_pattern=r'\w{1,}', ngram_range=(2,3), max_features=5000)
+tfidf_vect_ngram.fit(df_two_fields['claim'])
+xtrain_tfidf_ngram =  tfidf_vect_ngram.transform(train_x)
+xvalid_tfidf_ngram =  tfidf_vect_ngram.transform(valid_x)
+
+# characters level tf-idf
+tfidf_vect_ngram_chars = TfidfVectorizer(analyzer='char', token_pattern=r'\w{1,}', ngram_range=(2,3), max_features=5000)
+tfidf_vect_ngram_chars.fit(df_two_fields['claim'])
+xtrain_tfidf_ngram_chars =  tfidf_vect_ngram_chars.transform(train_x) 
+xvalid_tfidf_ngram_chars =  tfidf_vect_ngram_chars.transform(valid_x) 
+
+def train_model(classifier, feature_vector_train, label, feature_vector_valid):
+    classifier.fit(feature_vector_train, label)
+    predictions = classifier.predict(feature_vector_valid)
+    #print(accuracy_score(valid_y, predictions))
+    #print(confusion_matrix(valid_y, predictions))
+    #print(classification_report(valid_y, predictions))
+    list2 = valid_x.index
+    list3 = []  
+    list_predictions = list(predictions)
+    for i in range(0,len(list2)):
+        list3.append(str(str(list2[i]) + ", " + str(list_predictions[i])))
+        
+    np.savetxt("/usr/local/predictions.txt", list3, fmt='%s')
+
+#Logistic Regression
+#accuracy = train_model(linear_model.LogisticRegression(), xtrain_count, train_y, xvalid_count)
+
+accuracy = train_model(linear_model.LogisticRegression(), xtrain_tfidf, train_y, xvalid_tfidf)
+#accuracy = train_model(linear_model.LogisticRegression(), xtrain_tfidf_ngram, train_y, xvalid_tfidf_ngram)
+
+#accuracy = train_model(linear_model.LogisticRegression(), xtrain_tfidf_ngram_chars, train_y, xvalid_tfidf_ngram_chars)
+
