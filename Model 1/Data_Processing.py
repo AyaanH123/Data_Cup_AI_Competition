316aac8cd4bdfa9c5f05de964997a66a4d3b0d0b
dhruvpatel19 <53876897+dhruvpatel19@users.noreply.github.com>
2019-08-30 15:00:50 -0400

    Add files via upload

---
+++ Model 1/Data_Processing.py
@@ -0,0 +1,49 @@
+import numpy as np
+from keras.datasets import imdb
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers.embeddings import Embedding
+from keras.layers.convolutional import Conv1D, MaxPooling1D
+from keras.preprocessing import sequence
+from keras.models import load_model
+import Json_Read as jr
+from sklearn.feature_extraction.text import CountVectorizer
+# fix random seed for reproducibility
+np.random.seed(7)
+
+df_two_fields = jr.create_dataframes_claim_label('../TrueNorthAI')
+df_input = jr.create_dataframes_input('../TrueNorthAI')
+# load the dataset but only keep the top n words, zero the rest
+top_words = 5000
+train_x = df_two_fields['claim']
+train_y = df_two_fields['label']
+valid_x = df_input['claim']
+valid_y = df_input['label']
+vectorizer = CountVectorizer()
+train_x = vectorizer.fit_transform(train_x)
+valid_x = vectorizer.fit_transform(valid_x)
+# truncate and pad input sequences
+max_review_length = 21536
+train_x = sequence.pad_sequences(train_x.toarray(), maxlen=max_review_length)
+valid_x = sequence.pad_sequences(valid_x.toarray(), maxlen=max_review_length)
+# create the model
+embedding_vecor_length = 32
+model = Sequential()
+model.add(Embedding(top_words, embedding_vecor_length, input_length=max_review_length))
+model.add(Conv1D(filters=64, kernel_size=3, padding='same', activation='relu'))
+model.add(Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'))
+model.add(MaxPooling1D(pool_size=2))
+model.add(LSTM(100))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+print(model.summary())
+model.fit(train_x, train_y, epochs=3, batch_size=32)
+model.save('my_model.h5')  # creates a HDF5 file 'my_model.h5'
+del model  # deletes the existing model
+# returns a compiled model
+# identical to the previous one
+model = load_model('my_model.h5')
+# Final evaluation of the model
+scores = model.evaluate(valid_x, valid_y, verbose=0)
+print("Accuracy: %.2f%%" % (scores[1]*100))
